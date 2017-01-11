7b76f95dcafbb1b184b1ef059899ef3bdd715294
Martin Hagstrom <marhag87@gmail.com>
2017-01-11 15:33:39 +0100

    Use non-canary discord

--- discord-urgency.sh
+++ discord-urgency.sh
@@ -5,7 +5,7 @@ member=Notify
 
 dbus-monitor "interface='$interface',member='$member'" |
 while read -r line; do
-  if [[ $line == *'string "Discord Canary"'* ]]; then
+  if [[ $line == *'string "Discord"'* ]]; then
     xdotool search --onlyvisible --class discord set_window --urgency 1
   fi
 done
