d0e6aac94ad783efdc7d946f82a2482c36b734db
Martin Hagstrom <marhag87@gmail.com>
2017-11-27 21:47:49 +0100

    Set urgency even if discord is hidden

--- discord-urgency.sh
+++ discord-urgency.sh
@@ -6,6 +6,6 @@ member=Notify
 dbus-monitor "interface='$interface',member='$member'" |
 while read -r line; do
   if [[ $line == *'string "Discord"'* ]]; then
-    xdotool search --onlyvisible --class discord set_window --urgency 1
+    xdotool search --class discord set_window --urgency 1 %@
   fi
 done
